cd5edfddea3ae616fd4d1a89a1ed703e7cb34831
swaibuziraye22.github.io <swaibuziraye22@gmail.com>
2026-05-13 14:57:26 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,27 +1,28 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-import { getFirestore }
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-import { getAuth }
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-import { getStorage }
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+import {
+  getStorage
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 
-
-    // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDc7-XKk30DshUE-9PUoMF4VObt4UTIncM",
   authDomain: "zibuy-5deae.firebaseapp.com",
+  databaseURL: "https://zibuy-5deae-default-rtdb.firebaseio.com",
   projectId: "zibuy-5deae",
   storageBucket: "zibuy-5deae.firebasestorage.app",
   messagingSenderId: "283997357155",
   appId: "1:283997357155:web:de76cef72c6b278afda456"
-
-
 };
+
 
 const app = initializeApp(firebaseConfig);
 
@@ -31,8 +32,9 @@ const auth = getAuth(app);
 
 const storage = getStorage(app);
 
+
 export {
-    db,
-    auth,
-    storage
+  db,
+  auth,
+  storage
 };
