a6657f2ac2ecfcd91cbdc0c1236b4f01d88fc495
swaibuziraye22.github.io <swaibuziraye22@gmail.com>
2026-05-13 22:53:54 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 import {
   getAuth
