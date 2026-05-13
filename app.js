d8cbd8c4935154d870b7d2c8b65cb8f514730ac4
swaibuziraye22.github.io <swaibuziraye22@gmail.com>
2026-05-13 15:03:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,74 @@
 import { db, auth, storage } from "./firebase.js";
 
-console.log("ZiBuy connected successfully");
 
-console.log(db);
-console.log(auth);
-console.log(storage);
+const products = [
+
+  {
+    id: 1,
+    name: "Luxury Perfume",
+    price: 80000,
+    image: "https://picsum.photos/300/300?1"
+  },
+
+  {
+    id: 2,
+    name: "Smart Watch",
+    price: 120000,
+    image: "https://picsum.photos/300/300?2"
+  },
+
+  {
+    id: 3,
+    name: "Ladies Bag",
+    price: 95000,
+    image: "https://picsum.photos/300/300?3"
+  }
+
+];
+
+
+function renderProducts(){
+
+  const container = document.getElementById("products");
+
+  container.innerHTML = "";
+
+  products.forEach(product => {
+
+    container.innerHTML += `
+
+      <div class="product-card">
+
+        <img src="${product.image}" alt="${product.name}">
+
+        <h3>${product.name}</h3>
+
+        <p class="price">
+          UGX ${product.price.toLocaleString()}
+        </p>
+
+        <button onclick="addToCart(${product.id})">
+          Add To Cart
+        </button>
+
+      </div>
+
+    `;
+
+  });
+
+}
+
+
+window.addToCart = function(id){
+
+  const product = products.find(p => p.id === id);
+
+  if(!product) return;
+
+  alert(product.name + " added to cart");
+
+};
+
+
+renderProducts();
