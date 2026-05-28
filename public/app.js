494b74d0307d6e9aa87bfebb85a17edeeb445a84
swaibuziraye22.github.io <swaibuziraye22@gmail.com>
2026-05-28 15:54:20 +0100

    Update app.js

--- public/app.js
+++ public/app.js
@@ -936,30 +936,4 @@ window.allProducts = allProducts;
   }
 };
 
-let clickCount = 0;
-let clickTimer = null;
 
-document.addEventListener("DOMContentLoaded", () => {
-  const logo = document.getElementById("logo");
-
-  if (!logo) return;
-
-  logo.addEventListener("click", () => {
-    clickCount++;
-
-    clearTimeout(clickTimer);
-
-    clickTimer = setTimeout(() => {
-      clickCount = 0;
-    }, 1500); // reset if user stops clicking
-
-    if (clickCount === 5) {
-      clickCount = 0;
-      openAdminLogin();
-    }
-  });
-});
-
-function openAdminLogin() {
-  window.location.href = "admin.html";
-}
