fe39ff6d38d5c90d7666b70982817014965cb2a2
swaibuziraye22.github.io <swaibuziraye22@gmail.com>
2026-05-30 16:56:46 +0100

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1184,3 +1184,16 @@ function showSection(sectionId) {
 }
 
 window.showSection = showSection;
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then((reg) => {
+        console.log("Service Worker registered:", reg.scope);
+      })
+      .catch((err) => {
+        console.log("Service Worker failed:", err);
+      });
+  });
+}
