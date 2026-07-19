6613f896d4e0c4f07e5af37537a65f1a211832d6
swaibuziraye22.github.io <swaibuziraye22@gmail.com>
2026-07-19 23:59:46 +0100

    Update shop.js

--- public/shop.js
+++ public/shop.js
@@ -64,6 +64,22 @@ expireOldAds();
 /* ---------------- SHOP PRODUCTS ---------------- */
 let _shopAllProducts = [];
 
+
+// ======================================================
+// HTML Escape Helper
+// ======================================================
+function escapeHTML(str) {
+  if (str === null || str === undefined) return "";
+
+  return String(str)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
+
+
 async function loadShop() {
 
   const container =
